ce834dee3b4f9d1fdae5424fd0e45e9afe615944
Ryn Cao <cao7113@gmail.com>
2025-04-21 11:09:53 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from "next/font/google";
+
+// primary font
+export const inter = Inter({ subsets: ["latin"] });
+
+// secondary font
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+// console.log(`inter className: ${inter.className}`);
